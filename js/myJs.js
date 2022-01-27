142f4d766b23edb45d354f561f8163e7927b31c6
lema31204 <98501280+lema31204@users.noreply.github.com>
2022-01-27 09:41:06 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -1,17 +1,17 @@
 const textConfig = {
-  text1: "He luu cậu!",
-  text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
-  text3: "Cậu yêu tớ có phải không nào ._.",
-  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
-  text5: "Cậu mơ à???",
-  text6: "Yêu ơi là yêu <3",
-  text7: "lí do cậu thích tớ đi :vvvv",
-  text8: "Gửi cho tớ <3",
-  text9: "Vì cậu đẹp try vlllll",
-  text10: "Tớ biết mà ^^ Yêu cậu 300.000",
+  text1: "Hehe",
+  text2: "het buon chua.",
+  text3: "yeu thi hết buồn nha._.",
+  text4: "xin lỗi mà :v",
+  text5: "điên à???",
+  text6: "hết buồn ròi",
+  text7: "hứa nha :vvvv",
+  text8: "hong được bùn",
+  text9: "dạ",
+  text10: "nhắn toy đi",
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
-  text12: "Okii lunn <3",
+    "nhắn toy đi",
+  text12: "hee",
 };
 
 $(document).ready(function () {
